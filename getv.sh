8e928fbc1f60f09fcf9bc98ee937199ff2334442
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-17 13:49:16 -0800

    Update getv.sh

--- getv.sh
+++ getv.sh
@@ -6,6 +6,10 @@ cd .gitv
 git pull
 echo Building V..
 make
+if [ $? -ne 0 ]; then
+    echo "Error in building v"
+    exit 1
+fi
 echo Setting Up...
 cp -r vlib ..
 cp -r tools ..
@@ -14,10 +18,18 @@ cp -r examples ..
 cp v.v ..
 echo testing v
 ./v -prod -obf -o ../v v.v > /dev/null
+if [ $? -ne 0 ]; then
+    echo "V test failed"
+    exit 1
+fi
 cd ..
 ./v -o lossy v.v > /dev/null
+if [ $? -ne 0 ]; then
+    echo "V test failed"
+    exit 1
+fi
 rm lossy
 echo 
 echo Complete! Use ./v to use v, or add $PWD to path to use v anywhere!
-echo Use this script to update v too!
+echo Use this script to update v.
 echo
