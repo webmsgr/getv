c63a19a8662df467bcda303f0b0f32965929d450
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-17 13:37:08 -0800

    Update getv.sh

--- getv.sh
+++ getv.sh
@@ -10,8 +10,12 @@ cp -r vlib ..
 cp -r tools ..
 cp -r thirdparty ..
 cp -r examples ..
+cp v.v ..
 echo testing v
 ./v -prod -obf -o ../v v.v > /dev/null
+cd ..
+./v -o lossy v.v > /dev/null
+rm lossy
 echo 
 echo Complete! Use ./v to use v, or add $PWD to path to use v anywhere!
 echo Use this script to update v too!
