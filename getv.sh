c0002454be277767d9c8a3f8d8c7ea9712da9840
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-01-17 13:57:07 -0800

    Update getv.sh

--- getv.sh
+++ getv.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 echo GetV
+echo By webmsgr
+echo This tool is not endorsed by the makers of v
+echo
 echo Grabbing V...
-git clone https://github.com/vlang/v .gitv
+git clone https://github.com/vlang/v .gitv > /dev/null
 cd .gitv
 git pull
 echo Building V..
-make
+make > vbuild.log
 if [ $? -ne 0 ]; then
     echo "Error in building v"
+    cat vbuild.log
     exit 1
 fi
 echo Setting Up...
@@ -17,15 +21,17 @@ cp -r thirdparty ..
 cp -r examples ..
 cp v.v ..
 echo testing v
-./v -prod -obf -o ../v v.v > /dev/null
+./v -prod -obf -o ../v v.v > vstep1.log
 if [ $? -ne 0 ]; then
     echo "V test failed"
+    cat vstep1.log
     exit 1
 fi
 cd ..
-./v -o lossy v.v > /dev/null
+./v -o lossy v.v > vstep2.log
 if [ $? -ne 0 ]; then
     echo "V test failed"
+    cat vstep2.log
     exit 1
 fi
 rm lossy
